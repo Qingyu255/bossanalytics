0e624833d06be251d0615a01c2f0a7c87dfac381
Qingyu255 <qingyu2555@gmail.com>
2023-09-03 00:08:32 +0800

    first commit

---
+++ excel_merge.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import openpyxl
+
+
+def merge_excel_files(folder_path):
+    excel_files = [filename for filename in os.listdir(folder_path)]
+    entries = os.listdir(folder_path)
+
+    for entry in entries:
+        print(entry)
+
+    dfs = []
+    for i, filename in enumerate(excel_files):
+        file_path = os.path.join(folder_path, filename)
+        df = pd.read_excel(file_path, engine="xlrd")
+        if i == 0:
+            dfs.append(df)
+        else:
+            dfs.append(df[1:])
+
+    merged_df = pd.concat(dfs, ignore_index=True)
+    output_path = "/Users/qingyuliu/PycharmProjects/bossanalytics/data/merged_file.xls"
+    merged_df.to_excel(output_path, index=False, engine="openpyxl")
+
+
+folder_path = "/Users/qingyuliu/PycharmProjects/bossanalytics/data"
+merge_excel_files(folder_path)
